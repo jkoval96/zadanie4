61d203785c5cc10f086c724ae173007b47303550
Jan Koval <jan.koval96@gmail.com>
2017-05-08 11:49:24 +0200

    random,first try

--- src/zadanie4.c
+++ src/zadanie4.c
@@ -20,11 +20,26 @@ CTWL *ctwl_create_empty(void) {
 };
 
 CTWL *ctwl_create_random(unsigned int size) {
+	unsigned int i, rnd, tmp;
+	CTWL* random_ctwl = malloc(sizeof(*random_ctwl));
 	
+	for(i = 0; i < size; i++) {
+		rnd = rand() % 20;
+		random_ctwl->cur->data = rnd;
+		tmp = random_ctwl->cur->next;
+		random_ctwl->cur->next = tmp + i;
+	}
+	free(random_ctwl);
+	
+	return random_ctwl;
 };
 
 int main() {
+	srand(time(NULL));
+	unsigned int size = 10;
+	
 	CTWL *ctwl_create_empty(void);
+	CTWL *Cctwl_create_random(size);
 	
 	return 0;
 }
