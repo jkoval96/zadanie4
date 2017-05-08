9976d593146a3f737efa994510e866fb4b856a2a
Jan Koval <jan.koval96@gmail.com>
2017-05-08 15:54:54 +0200

    first build of new functions

--- src/zadanie4.c
+++ src/zadanie4.c
@@ -12,7 +12,7 @@ typedef struct {
 }CTWL;
 
 CTWL *ctwl_create_empty(void) {	
-	CTWL *start = malloc(sizeof(*start));
+	CTWL *start = malloc(sizeof(CTWL *));
 	start->cur = NULL;
 	free(start);
 	
@@ -20,16 +20,37 @@ CTWL *ctwl_create_empty(void) {
 };
 
 void ctwl_cur_step_right(CTWL *list){
-	
-	list = malloc(sizeof(*list));
-	
+	list = malloc(sizeof(CTWL *));
+	list->cur = list->cur->next;
 	free(list);
+};
+
+void ctwl_cur_step_left(CTWL *list) {
+	list = malloc(sizeof(CTWL *));
+	list->cur = list->cur->prev;
+	free(list);
+};
+
+TWN *ctwl_insert_left(CTWL* list, float val){
+	list = malloc(sizeof(CTWL *));
+	ctwl_cur_step_left(list);
+	list->cur->data = val;
+	free(list);	
+	
+	//return list->cur->data = val;
+};
+
+TWN *ctwl_insert_right(CTWL* list, float val){
+	list = malloc(sizeof(CTWL *));
+	list->cur = list->cur->next;
+	list->cur->data = val;
+	free(list);	
 };
 
 CTWL *ctwl_create_random(unsigned int size) {
 	unsigned int i, tmp;
 	float rnd;
-	CTWL *random_ctwl = malloc(sizeof(*random_ctwl));
+	CTWL *random_ctwl = malloc(sizeof(CTWL *));
 	
 	for(i = 0; i < size; i++) {
 		rnd = rand() % 20;
